dc31e8b705cddeb1ea3c21bf009b93be60fa428d
Vetrivel D <vetrivel.dhandapani@gmail.com>
2017-10-13 18:55:54 +0530

    Added Dashboard Bageds Dynamic

--- Licensing/src/app/component/dashboard/dashboard.component.ts
+++ Licensing/src/app/component/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Dashboard, Badge } from '../../models/dashboards.models'
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  public dashboard = new Dashboard();
+  
   constructor() { }
 
   ngOnInit() {
+    /* Mack Data */
+    this.dashboard.Badges = [];
+    this.dashboard.Badges.push({Id:1,Name:"Products",Icon:"fa fa-users fa-4x",Counts:10,Link1:"/manage-product",Link2:"/",Link3:"/"});
+    this.dashboard.Badges.push({Id:2,Name:"Customers",Icon:"fa fa-shopping-cart fa-4x",Counts:200,Link1:"/",Link2:"/",Link3:"/"});
+    this.dashboard.Badges.push({Id:3,Name:"Sales",Icon:"fa fa-usd fa-4x",Counts:1895,Link1:"/",Link2:"/",Link3:"/"});
+    this.dashboard.Badges.push({Id:3,Name:"Renewals",Icon:"fa fa-eye fa-4x",Counts:256,Link1:"/",Link2:"/",Link3:"/"});
+    
   }
 
 }
---
+++ Licensing/src/app/models/dashboards.models.ts
@@ -0,0 +1,13 @@
+export class Dashboard {
+    Badges: Badge[];
+}
+export class Badge{
+    Id: Number;
+    Name: string;
+    Icon: string;
+    Link1: string;
+    Link2: string;
+    Link3: string;
+    Counts: Number;
+
+}
