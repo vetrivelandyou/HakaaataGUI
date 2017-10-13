90c345042b689825d973ed3c75f707d7ac1e4ec3
Vetrivel D <vetrivel.dhandapani@gmail.com>
2017-10-13 15:28:39 +0530

    Added Some Files

--- Licensing/src/app/app.module.ts
+++ Licensing/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { HeaderBarComponent } from './component/header-bar/header-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderBarComponent
   ],
   imports: [
     BrowserModule
